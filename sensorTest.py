6b579401a54083ba76e16d982137859001b18558
wkk0407 <blueseakr24@gmail.com>
2020-01-29 10:23:01 -0800

    fix

--- sensorTest.py
+++ sensorTest.py
@@ -11,7 +11,7 @@ GPIO.setup(ECHO,GPIO.IN)
 
 GPIO.output(TRIG, False)
 
-print "Waiting For Sensor To Settle"
+print("Waiting For Sensor To Settle")
 
 time.sleep(2)
 
@@ -30,7 +30,7 @@ while GPIO.input(ECHO)==1:
   pulse_end = time.time()       
 
 pulse_duration = pulse_end - pulse_start
-distance = pulse_duration x 17150
+distance = pulse_duration * 17150
 distance = round(distance, 2)
-print "Distance:",distance,"cm"
+print("Distance:",distance,"cm")
 GPIO.cleanup()  
