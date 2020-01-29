ae38e5552b3d0f3853ec5b25f00880530deb7950
gradythompson <grady_thompson@outlook.com>
2020-01-29 10:07:05 -0800

    Added functions and function descriptions

---
+++ GUI.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from tkinter import ttk
+import time
+import math
+import matplotlib
+
+'''
+The main window of the GUI
+Creates the main window of the GUI has 4 columns and 8 rows
+Contains the grph, option based features, radio button to determine feature and submission box to change rate of measurement
+'''
+def mainWindow ():
+    return
+
+'''
+Graph showing the current distances and angles inside a canvas
+Has markers for the distance and angle
+Left of window
+'''
+def graph ():
+    return
+
+'''
+Fixed option features
+Has option to change the angle the servo is at
+Displays the distance, velocity and acceleration
+Bottom right of window
+'''
+def fixed ():
+    return
+
+'''
+Sweeping option features
+Has a Semi-circle showing most recent measurements in each direction
+Bottom right of window
+'''
+def sweeping ():
+    return
