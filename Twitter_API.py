ad6ef1bcc642c07a4cce4f3cc317f18ef5118ee5
Vincent <vincentyan8@hotmail.com>
2020-01-30 00:52:23 -0800

    Started on Twitter API: Finished the code necessary to start tweeting
    required: Twitter account keys

---
+++ Twitter_API.py
@@ -0,0 +1,42 @@
+from twython import Twython
+from twython import TwythonStreamer
+from auth import (
+    consumer_key, consumer_secret, access_token, access_token_secret
+)
+
+
+# Listens out for things
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            username = data['user']['screen_name']
+            tweet = data['text']
+            print("@{}: {}".format(username, tweet))
+
+
+stream = MyStreamer(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+stream.statuses.filter(track='raspberry pi')
+# Listens for the track keyword and on success prints out the
+# Screen Name and full text
+
+twitter = Twython(
+    consumer_secret,
+    consumer_key,
+    access_token_secret,
+    access_token
+)
+# Uses the API's update_status() function to send a tweet containing hello twitter
+message = "Hello Twitter!"
+twitter.update_status(status=message)
+print("Tweeted: %s" % message)
+
+# Code to upload image to Twitter
+image = open('image.jpg', 'rb')  # file opens the image
+response = twitter.upload_media(media=image)
+media_id = [response['media_id']]
+twitter.update_status(status=message, media_ids=media_id)
