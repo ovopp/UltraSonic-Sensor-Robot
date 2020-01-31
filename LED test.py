a65efbca3ac1fda27dd8a0d72a5cab6ab06851b7
Vincent <vincentyan8@hotmail.com>
2020-01-30 21:31:29 -0800

    Update gpio

--- LED test.py
+++ LED test.py
@@ -9,23 +9,23 @@ GPIO.setup(21,GPIO.OUT)
 
 while 1:                               # execute loop forever
     for y in range(4):            # loop for counting up 8 times
-        GPIO.output(4, 1)            # pull up the data pin for every bit.
+        GPIO.output(16, 1)            # pull up the data pin for every bit.
         time.sleep(0.1)            # wait for 100ms
-        GPIO.output(5,1)            # pull CLOCK pin high
+        GPIO.output(20,1)            # pull CLOCK pin high
         time.sleep(0.1)
-        GPIO.output(5,0)            # pull CLOCK pin down, to send a rising edge
-        GPIO.output(4,0)            # clear the DATA pin
-        GPIO.output(6,1)            # pull the SHIFT pin high to put the 8 bit data out parallel
+        GPIO.output(20,0)            # pull CLOCK pin down, to send a rising edge
+        GPIO.output(16,0)            # clear the DATA pin
+        GPIO.output(21,1)            # pull the SHIFT pin high to put the 8 bit data out parallel
         time.sleep(0.1)
-        GPIO.output(6,0)            # pull down the SHIFT pin
+        GPIO.output(21,0)            # pull down the SHIFT pin
 
     for y in range(4):            # loop for counting up 8 times
-        GPIO.output(4,0)            # clear the DATA pin, to send 0
+        GPIO.output(16,0)            # clear the DATA pin, to send 0
         time.sleep(0.1)            # wait for 100ms
-        GPIO.output(5,1)            # pull CLOCK pin high
+        GPIO.output(20,1)            # pull CLOCK pin high
         time.sleep(0.1)
-        GPIO.output(5,0)            # pull CLOCK pin down, to send a rising edge
-        GPIO.output(4,0)            # keep the DATA bit low to keep the countdown
-        GPIO.output(6,1)            # pull the SHIFT pin high to put the 8 bit data out parallel
+        GPIO.output(20,0)            # pull CLOCK pin down, to send a rising edge
+        GPIO.output(16,0)            # keep the DATA bit low to keep the countdown
+        GPIO.output(21,1)            # pull the SHIFT pin high to put the 8 bit data out parallel
         time.sleep(0.1)
-        GPIO.output(6,0)
+        GPIO.output(21,0)
