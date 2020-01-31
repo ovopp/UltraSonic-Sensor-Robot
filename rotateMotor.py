187a3de4e3fa72bd042fdc32040e24d4f56ada69
Vincent <51847043+ovopp@users.noreply.github.com>
2020-01-30 23:03:35 -0800

    LED's working, motor working, sensor working

--- rotateMotor.py
+++ rotateMotor.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)           # do not show any warnings
 
@@ -17,70 +19,6 @@ GPIO.setup(21,GPIO.OUT)
 
 p = GPIO.PWM(SERVO, 50) # GPIO 18 for PWM with 50Hz
 p.start(2.5)
-
-
-def distance(i):
-  p.ChangeDutyCycle(2.5 + 1/6 * i)
-  GPIO.output(TRIGGER, False)
-  time.sleep(0.06)
-  GPIO.output(TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(TRIGGER, False)
-  while GPIO.input(ECHO)==0:
-    start = time.time()
-  while GPIO.input(ECHO)==1:
-    stop = time.time()
-  elapsed = stop-start
-  distance = (elapsed * 34000.0) / 2
-  if (distance < 15):
-    ledController(1)
-  elif (distance <30):
-    ledController(2)
-  elif (distance < 45):
-    ledController(3)
-  else:
-    ledController(4)
-  print("Angle: ", i * 3)
-  print("Distance : %.1f cm" % distance)
-
-def distanceBack(i):
-  p.ChangeDutyCycle(12.5 - 1/6 * i)
-  GPIO.output(TRIGGER, False)
-  time.sleep(0.06)
-  GPIO.output(TRIGGER, True)
-  time.sleep(0.00001)
-  GPIO.output(TRIGGER, False)
-  while GPIO.input(ECHO)==0:
-    start = time.time()
-  while GPIO.input(ECHO)==1:
-    stop = time.time()
-  elapsed = stop-start
-  distance = (elapsed * 34000.0) / 2
-  if (distance < 15):
-    ledController(1)
-  elif (distance <30):
-    ledController(2)
-  elif (distance < 45):
-    ledController(3)
-  else:
-    ledController(4)
-  print("Angle: ", i * 3)
-  print("Distance : %.1f cm" % distance)
-
-try:
-  while True:
-    for i in range(20):
-      p.ChangeDutyCycle(2.5)
-      distance(i)
-    for i in range(20):
-      p.ChangeDutyCycle(12.5)
-      distanceBack(i)
-
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
-
-
 
 def ledController(x):
     GPIO.output(16,0) #DATA PIN set to 0
@@ -102,4 +40,68 @@ def ledController(x):
     GPIO.output(21,1)            # pull up the SHIFT pin
     time.sleep(0.001)
     GPIO.output(21,0)            # pull down the SHIFT pin
+
+
+def distance(i):
+  p.ChangeDutyCycle(2.5 + 1/6 * i)
+  GPIO.output(TRIGGER, False)
+  time.sleep(0.06)
+  GPIO.output(TRIGGER, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIGGER, False)
+  while GPIO.input(ECHO)==0:
+    start = time.time()
+  while GPIO.input(ECHO)==1:
+    stop = time.time()
+  elapsed = stop-start
+  distance = (elapsed * 34000.0) / 2
+  if (distance < 15):
+    ledController(2)
+  elif (distance <30):
+    ledController(3)
+  elif (distance < 45):
+    ledController(4)
+  else:
+    ledController(5)
+  print("Angle: ", i * 3)
+  print("Distance : %.1f cm" % distance)
+
+def distanceBack(i):
+  p.ChangeDutyCycle(12.5 - 1/6 * i)
+  GPIO.output(TRIGGER, False)
+  time.sleep(0.06)
+  GPIO.output(TRIGGER, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIGGER, False)
+  while GPIO.input(ECHO)==0:
+    start = time.time()
+  while GPIO.input(ECHO)==1:
+    stop = time.time()
+  elapsed = stop-start
+  distance = (elapsed * 34000.0) / 2
+  if (distance < 15):
+    ledController(2)
+  elif (distance <30):
+    ledController(3)
+  elif (distance < 45):
+    ledController(4)
+  else:
+    ledController(5)
+  print("Angle: ", i * 3)
+  print("Distance : %.1f cm" % distance)
+
+try:
+  while True:
+    for i in range(60):
+      p.ChangeDutyCycle(2.5)
+      distance(i)
+    for i in range(60):
+      p.ChangeDutyCycle(12.5)
+      distanceBack(i)
+
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
+
+
 
