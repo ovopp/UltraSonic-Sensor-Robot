3e6c1041ea3fee5152c06e2144784205e8cee1cc
wkk0407 <blueseakr24@gmail.com>
2020-01-30 19:11:38 -0800

    fixed sensor and rotate.

--- sensorTest.py
+++ sensorTest.py
@@ -3,34 +3,34 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-TRIG = 23
+TRIGGER = 23
 ECHO = 24
-print "Distance Measurement In Progress"
-GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(TRIGGER,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
+try:
+  while True:
+    # turn off the trigger pin
+    GPIO.output(TRIGGER, False)
+    time.sleep(2)
+    # turn on the trigger pin
+    GPIO.output(TRIGGER, True)
+    #send out the 10us pulse
+    time.sleep(0.00001)
+    # turn off the trigger pin
+    GPIO.output(TRIGGER, False)
+    # set the time 
+    while GPIO.input(ECHO)==0:
+      start = time.time()
+    while GPIO.input(ECHO)==1:
+      stop = time.time()
+    # calculate pulse length
+    elapsed = stop-start
+    # calculate the distance
+    distance = (elapsed * 34000.0) / 2
+    print("Distance : %.1f cm" % distance)
+except KeyboardInterrupt:   
+    print "Ultrasonic Distance Measurement End" 
+    GPIO.cleanup()
 
-print("Waiting For Sensor To Settle")
-
-time.sleep(2)
-
-GPIO.output(TRIG, True)
-
-time.sleep(0.00001)
-
-GPIO.output(TRIG, False)
-
-while GPIO.input(ECHO)==0:
-
-  pulse_start = time.time()
-
-while GPIO.input(ECHO)==1:
-
-  pulse_end = time.time()       
-
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 17150
-distance = round(distance, 2)
-print("Distance:",distance,"cm")
-GPIO.cleanup()  
+GPIO.cleanup()
