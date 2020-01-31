c08d557e1ea053c2dc603ac2c10f6a303790ecf8
Vincent <51847043+ovopp@users.noreply.github.com>
2020-01-31 09:37:21 -0800

    Final commit for Demo

--- GUI.py
+++ GUI.py
@@ -50,7 +50,7 @@ temp = 23  # default temp
 
 # Thread variables
 on = False
-speed = 5
+speed = 7
 
 
 def tweet(message):
@@ -107,15 +107,12 @@ def distanceStart():
     global TweetDistance, temp, speed
     print("In distance start")
     for j in range(60):
-        #Rotates the servo
-        # we are rotating 3 degrees a time,
-        # 180/3 = 60 changes, 10/60 = 1/6 dutycycle change
+        #Rotates the servo 3 degrees
         p.ChangeDutyCycle(2.5 + 1 / 6 * j)
         #Sends a pulse of sound
         GPIO.output(TRIGGER, False)
         time.sleep(1-0.094*speed)
         GPIO.output(TRIGGER, True)
-        # send out the 10us pulse
         time.sleep(0.00001)
         GPIO.output(TRIGGER, False)
         #Measures the start and stop of the echo wave
@@ -150,15 +147,12 @@ def distanceBack():
     print("in distance back")
     #Measures every 3 degrees of servo rotation
     for k in range(60):
-        #Rotate servo 
-        # we are rotating 3 degrees a time,
-        # 180/3 = 60 changes, 10/60 = 1/6 dutycycle change
+        #Rotate servo
         p.ChangeDutyCycle(12.5 - 1 / 6 * k)
         #Sends pulse
         GPIO.output(TRIGGER, False)
         time.sleep(1-0.094*speed)
         GPIO.output(TRIGGER, True)
-        # send out the pulse
         time.sleep(0.00001)
         GPIO.output(TRIGGER, False)
         #Measures start and stop of the echo
@@ -194,17 +188,17 @@ def drawDist(angle, length):
             
     if length > 70:
         length = 70 #max size length can be without making it like huge
-        
+    length *= 2.7
     if len(linelist) == 61:
         radar.delete(linelist[index])
-        line = radar.create_line(145 + length * math.sin(angle - math.pi / 2), 145 - length * math.cos(angle - math.pi / 2),
-                      145, 145, fill="green")
+        line = radar.create_line(290 + length * math.sin(angle - math.pi / 2), 290 - length * math.cos(angle - math.pi / 2),
+                      290, 290, fill="green")
         linelist[index] = line
     
     else:
         #Draws a line at the specified angle and distance on the radar
-        line = radar.create_line(145 + length * math.sin(angle - math.pi / 2), 145 - length * math.cos(angle - math.pi / 2),
-                      145, 145, fill="green")
+        line = radar.create_line(290 + length * math.sin(angle - math.pi / 2), 290 - length * math.cos(angle - math.pi / 2),
+                      290, 290, fill="green")
         linelist.append(line)
 
 
@@ -212,7 +206,7 @@ def drawDist(angle, length):
 def tweetEvent():
     print("tweet event")
     if TweetDistance < 10:
-        tweet("Current Distance: " + str(TweetDistance))
+        tweet("Current Distance: " + str(TweetDistance) + " cm. You're too close!")
     return
 
 #Changes the speed of the servo motor
@@ -238,7 +232,6 @@ thread = threading.Thread(target=startServo, args=())
 thread.daemon = True
 thread.start()
 
-
 try:
     '''
     The main window of the GUI
@@ -249,7 +242,7 @@ try:
 
     # Creates window
     window = Tk()
-    window.geometry("1000x500")
+    window.geometry("1300x500")
     window.title("Sonar Interface")  # sets title
     # Allows for resizing
     for i in range(10):
@@ -257,97 +250,97 @@ try:
         window.rowconfigure(i, weight=1)
 
     # Creates radar canvas
-    radar = Canvas(window, height="300", width="300")
+    radar = Canvas(window, height="400", width="700")
 
     # Adds radar arc
-    mainArc = 50, 50, 240, 240
+    mainArc = 100, 100, 480, 480
     arcOuter = radar.create_arc(mainArc, start=0, extent=180, fill="black", outline="white")
 
     '''Reference curves: The curves are used to easily measure the distance that the sensor detects'''
-    shift1 = 550 / 7
-    innerArc1 = 50 + shift1, 50 + shift1, 240 - shift1, 240 - shift1
+    shift1 = 2*551 / 7
+    innerArc1 = 100 + shift1, 100 + shift1, 480 - shift1, 480 - shift1
     arcInner1 = radar.create_arc(innerArc1, start=0, extent=180, outline="white", fill="")
 
-    shift2 = 460 / 7
-    innerArc2 = 50 + shift2, 50 + shift2, 240 - shift2, 240 - shift2
+    shift2 = 2*460 / 7
+    innerArc2 = 100 + shift2, 100 + shift2, 480 - shift2, 480 - shift2
     arcInner2 = radar.create_arc(innerArc2, start=0, extent=180, outline="white", fill="")
 
-    shift3 = 370 / 7
-    innerArc3 = 50 + shift3, 50 + shift3, 240 - shift3, 240 - shift3
+    shift3 = 2*370 / 7
+    innerArc3 = 100 + shift3, 100 + shift3, 480 - shift3, 480 - shift3
     arcInner3 = radar.create_arc(innerArc3, start=0, extent=180, outline="white", fill="")
 
-    shift4 = 280 / 7
-    innerArc4 = 50 + shift4, 50 + shift4, 240 - shift4, 240 - shift4
+    shift4 = 2*280 / 7
+    innerArc4 = 100 + shift4, 100 + shift4, 480 - shift4, 480 - shift4
     arcInner4 = radar.create_arc(innerArc4, start=0, extent=180, outline="white", fill="")
 
-    shift5 = 190 / 7
-    innerArc5 = 50 + shift5, 50 + shift5, 240 - shift5, 240 - shift5
+    shift5 = 2*190 / 7
+    innerArc5 = 100 + shift5, 100 + shift5, 480 - shift5, 480 - shift5
     arcInner5 = radar.create_arc(innerArc5, start=0, extent=180, outline="white", fill="")
 
-    shift6 = 100 / 7
-    innerArc6 = 50 + shift6, 50 + shift6, 240 - shift6, 240 - shift6
+    shift6 = 2*100 / 7
+    innerArc6 = 100 + shift6, 100 + shift6, 480 - shift6, 480 - shift6
     arcInner6 = radar.create_arc(innerArc6, start=0, extent=180, outline="white", fill="")
 
     '''Reference lines: Lines used to measure the angle of the measurements'''
-    distance = 70 * 1.35  # The distance of the lines used for reference (maximum)
+    distance = 70 * 2.7  # The distance of the lines used for reference (maximum)
 
     angle1 = math.pi / 4
-    AngleRef1 = radar.create_line(145 + distance * math.sin(angle1 - math.pi / 2),
-                                  145 - distance * math.cos(angle1 - math.pi / 2), 145, 145, fill="white")
+    AngleRef1 = radar.create_line(290 + distance * math.sin(angle1 - math.pi / 2),
+                                  290 - distance * math.cos(angle1 - math.pi / 2), 290, 290, fill="white")
 
     angle2 = math.pi / 2
-    AngleRef2 = radar.create_line(145 + distance * math.sin(angle2 - math.pi / 2),
-                                  145 - distance * math.cos(angle2 - math.pi / 2), 145, 145, fill="white")
+    AngleRef2 = radar.create_line(290 + distance * math.sin(angle2 - math.pi / 2),
+                                  290 - distance * math.cos(angle2 - math.pi / 2), 290, 290, fill="white")
 
     angle3 = 3 * math.pi / 4
-    AngleRef3 = radar.create_line(145 + distance * math.sin(angle3 - math.pi / 2),
-                                  145 - distance * math.cos(angle3 - math.pi / 2), 145, 145, fill="white")
+    AngleRef3 = radar.create_line(290 + distance * math.sin(angle3 - math.pi / 2),
+                                  290 - distance * math.cos(angle3 - math.pi / 2), 290, 290, fill="white")
 
     '''Reference labels: The values of each of the reference curves and arcs'''
     # Angle labels
     angleLabel1 = Label(radar, text="0*")
     angleLabel1.pack()
-    radar.create_window(30, 140, window=angleLabel1)
+    radar.create_window(60, 280, window=angleLabel1)
 
     angleLabel2 = Label(radar, text="45*")
     angleLabel2.pack()
-    radar.create_window(70, 60, window=angleLabel2)
+    radar.create_window(140, 120, window=angleLabel2)
 
     angleLabel3 = Label(radar, text="90*")
     angleLabel3.pack()
-    radar.create_window(150, 40, window=angleLabel3)
+    radar.create_window(300, 80, window=angleLabel3)
 
     angleLabel4 = Label(radar, text="135*")
     angleLabel4.pack()
-    radar.create_window(220, 60, window=angleLabel4)
+    radar.create_window(440, 120, window=angleLabel4)
 
     angleLabel5 = Label(radar, text="180*")
     angleLabel5.pack()
-    radar.create_window(270, 140, window=angleLabel5)
+    radar.create_window(540, 280, window=angleLabel5)
 
     # Distance labels
     distanceLabel1 = Label(radar, text="0")
     distanceLabel1.pack()
-    radar.create_window(145, 155, window=distanceLabel1)
+    radar.create_window(290, 310, window=distanceLabel1)
 
     distanceLabel2 = Label(radar, text="20")
     distanceLabel2.pack()
-    radar.create_window(175, 155, window=distanceLabel2)
+    radar.create_window(350, 310, window=distanceLabel2)
 
     distanceLabel3 = Label(radar, text="40")
     distanceLabel3.pack()
-    radar.create_window(200, 155, window=distanceLabel3)
+    radar.create_window(400, 310, window=distanceLabel3)
 
     distanceLabel4 = Label(radar, text="60")
     distanceLabel4.pack()
-    radar.create_window(225, 155, window=distanceLabel4)
+    radar.create_window(450, 310, window=distanceLabel4)
 
     radar.grid(row=1, rowspan=2, column=1)
 
     '''Twitter API interactions'''
     # Label describing tweet conditions
     tweetLabel = Label(window,
-                       text="This button will tweet if the distance sensor is reportin a distance less than 10cm")
+                       text="This button will tweet if the distance sensor \n is reportin a distance less than 10cm")
     tweetLabel.grid(column=2, row=1, sticky="S")
 
     # Button that sends tweet if the conditions are met
@@ -368,7 +361,20 @@ try:
     window.mainloop()
 
 except KeyboardInterrupt:
+    window.destroy()
     p.stop()
+    GPIO.output(16, 0)  # DATA PIN set to 0
+    time.sleep(0.001)
+    # Resets the memory in the LEDs
+    for l in range(4):
+        GPIO.output(20, 1)  # pull CLOCK pin high
+        time.sleep(0.001)
+        GPIO.output(20, 0)
+    GPIO.output(21, 1)  # pull up the SHIFT pin
+    time.sleep(0.001)
+    GPIO.output(21, 0)
     GPIO.cleanup()
+    
+    print("program done")
 
 
