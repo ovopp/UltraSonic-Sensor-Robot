85b89744986eb39cc49320b73075a0258b36d2a2
GradyThompson <grady_thompson@outlook.com>
2020-01-30 23:32:24 -0800

    Added ability to interact with Rpi

--- GUI.py
+++ GUI.py
@@ -3,27 +3,88 @@ from tkinter import ttk
 import time
 import math
 import matplotlib
-#import Twitter_API
+import Twitter_API
+import rotateMotor
+import RPi.GPIO as GPIO
+import time
 window = Tk();
 
-'''
-The main window of the GUI
-Creates the main window of the GUI has 4 columns and 8 rows
-Contains the grph, option based features, radio button to determine feature and submission box to change rate of measurement
-'''
-def mainWindow ():
-    #Draws a distance line on the canvas
-    def drawDist(angle, distance):
-        distance*=1.35
-        radar.create_line(145 + distance*math.sin(angle - math.pi/2), 145 - distance*math.cos(angle - math.pi/2), 145, 145, fill="green")
+GPIO.setmode(GPIO.BCM)
+
+SERVO = 18
+TRIGGER = 23
+ECHO = 24
+
+GPIO.setup(SERVO, GPIO.OUT)
+GPIO.setup(TRIGGER,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+p = GPIO.PWM(SERVO, 50) # GPIO 18 for PWM with 50Hz
+p.start(2.5)
+
+def distance(i):
+  p.ChangeDutyCycle(2.5 + 1/6 * i)
+  GPIO.output(TRIGGER, False)
+  time.sleep(0.06)
+  GPIO.output(TRIGGER, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIGGER, False)
+  while GPIO.input(ECHO)==0:
+    start = time.time()
+  while GPIO.input(ECHO)==1:
+    stop = time.time()
+  elapsed = stop-start
+  distance = (elapsed * 34000.0) / 2
+  print("Angle: ", i * 3)
+  print("Distance : %.1f cm" % distance)
+  TweetDistance = distance;
+  drawDist(angle, distance)
+
+def distanceBack(i):
+  p.ChangeDutyCycle(12.5 - 1/6 * i)
+  GPIO.output(TRIGGER, False)
+  time.sleep(0.06)
+  GPIO.output(TRIGGER, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIGGER, False)
+  while GPIO.input(ECHO)==0:
+    start = time.time()
+  while GPIO.input(ECHO)==1:
+    stop = time.time()
+  elapsed = stop-start
+  distance = (elapsed * 34000.0) / 2
+  print("Angle: ", i * 3)
+  print("Distance : %.1f cm" % distance)
+  TweetDistance = distance;
+  drawDist(angle, distance)
+  
+#Draws a distance line on the canvas
+def drawDist(angle, distance):
+    distance*=1.35
+    radar.create_line(145 + distance*math.sin(angle - math.pi/2), 145 - distance*math.cos(angle - math.pi/2), 145, 145, fill="green")
+
+#If the distance is less than 10cm then it tweets the current distance
+def tweetEvent ():
+    if (distance < 10):
+        Twitter_API.tweet("Current Distance: " + str(TweetDistance))
+    return
+
+try:
+    global TweetDistance = 70
+    '''
+    The main window of the GUI
+    Creates the main window of the GUI has 4 columns and 8 rows
+    Contains the grph, option based features, radio button to determine feature and submission box to change rate of measurement
+    '''
 
     #Creates window
     window.geometry("1000x500")
     window.title("Sonar Interface")  # sets title
+    #Allows for resizing
     for i in range(10):
         window.columnconfigure(i, weight=1)
         window.rowconfigure(i, weight=1)
-
+          
     #Creates radar canvas
     radar = Canvas (window, height="300", width="300")
 
@@ -31,12 +92,7 @@ def mainWindow ():
     mainArc = 50, 50, 240, 240
     arcOuter = radar.create_arc(mainArc, start=0, extent=180, fill="black", outline="white")
 
-    for i in range(60):
-        drawDist(i*math.pi/60, 20);
-        drawDist(i*math.pi/60+math.pi/180, 20);
-        drawDist(i*math.pi/60+2*math.pi/180, 20);
-
-    '''Reference curves'''
+    '''Reference curves: The curves are used to easily measure the distance that the sensor detects'''
     shift1 = 550/7
     innerArc1 = 50+shift1, 50+shift1, 240-shift1, 240-shift1
     arcInner1 = radar.create_arc(innerArc1, start=0, extent=180, outline="white", fill="")
@@ -53,7 +109,7 @@ def mainWindow ():
     shift4 = 280/7
     innerArc4 = 50+shift4, 50+shift4, 240-shift4, 240-shift4
     arcInner4 = radar.create_arc(innerArc4, start=0, extent=180, outline="white", fill="")
-    
+
     shift5 = 190/7
     innerArc5 = 50+shift5, 50+shift5, 240-shift5, 240-shift5
     arcInner5 = radar.create_arc(innerArc5, start=0, extent=180, outline="white", fill="")
@@ -64,8 +120,8 @@ def mainWindow ():
 
     
     '''Reference lines'''
-    distance = 70*1.35
-    
+    distance = 70*1.35 #The distance of the lines used for reference (maximum)
+
     angle1 = math.pi/4
     AngleRef1 = radar.create_line(145 + distance*math.sin(angle1 - math.pi/2), 145 - distance*math.cos(angle1 - math.pi/2), 145, 145, fill="white")
 
@@ -117,14 +173,25 @@ def mainWindow ():
     radar.create_window(225, 155, window=distanceLabel4)
     
     radar.grid(row=1, rowspan=2, column=1)
-
+    
     '''Twitter API interactions'''
+    #Label describing tweet conditions
     tweetLabel = Label(window, text="This button will tweet if the distance sensor is reportin a distance less than 10cm")
     tweetLabel.grid(column=2, row=1, sticky="S")
     
-    tweetButton = Button(window, text="TWEET")
+    #Button that sends tweet if the conditions are met
+    tweetButton = Button(window, text="TWEET", command=tweetEvent, width="20")
     tweetButton.grid(column=2, row=2, sticky="N")
-    window.mainloop()
-    return
 
-mainWindow()
+    for i in range(20):
+      p.ChangeDutyCycle(2.5)
+      distance(i)
+    for i in range(20):
+      p.ChangeDutyCycle(12.5)
+      distanceBack(i)
+    
+    #The main window loop
+    window.mainloop()
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
